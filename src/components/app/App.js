6dee48f8524bfa14e6467567ef66dd4f5b1f0ea2
dr_cote <merezhin@gmail.com>
2022-01-10 00:06:40 +0300

    Добавляет работу с сервером по загрузке данных

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,23 +2,42 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
-
 import decoration from '../../resources/img/vision.png';
+import { Component } from "react";
+import ErrorBoundariy from "../errorBoudariy/ErrorBoundariy";
 
-const App = () => {
-    return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
-    )
+class App extends Component {
+    state = {
+        selectedChar: null
+    }
+
+    onCharSelected = (id) => {
+        this.setState({
+            selectedChar: id
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <ErrorBoundariy>
+                        <RandomChar />
+                    </ErrorBoundariy>
+                    <div className="char__content">
+                        <ErrorBoundariy>
+                            <CharList onCharSelected={this.onCharSelected} />
+                        </ErrorBoundariy>
+                        <ErrorBoundariy>
+                            <CharInfo charId={this.state.selectedChar} />
+                        </ErrorBoundariy>
+                    </div>
+                    <img className="bg-decoration" src={decoration} alt="vision" />
+                </main>
+            </div>
+        )
+    }
 }
 
 export default App;
