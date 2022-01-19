71a2c0d17a345e6c3f1bbc3ee99222a4b25970e0
dr_cote <merezhin@gmail.com>
2022-01-19 15:56:48 +0300

    Добавляет фокус для элементов

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -58,8 +58,20 @@ class CharList extends Component {
         })
     }
 
+    itemRefs = [];
+
+    setRefItem = (item) => {
+        this.itemRefs.push(item);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
     renderItems(arr) {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             let imgStyle = { 'objectFit': 'cover' };
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = { 'objectFit': 'unset' };
@@ -67,9 +79,14 @@ class CharList extends Component {
 
             return (
                 <li
+                    tabIndex={0}
+                    ref={this.setRefItem}
                     className="char__item"
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}>
                     <img src={item.thumbnail} alt={item.name} style={imgStyle} />
                     <div className="char__name">{item.name}</div>
                 </li>
@@ -114,7 +131,7 @@ class CharList extends Component {
     }
 }
 
-CharList.propTypes={
+CharList.propTypes = {
     onCharSelected: PropTypes.func
 }
 
